5c8f0ccde46882a68bb0621e0ed9b0627a123dcd
SAI_KUMAR_ALLA <2000avsaikumar@gmail.com>
2025-05-13 13:12:17 +0530

    dvc chnages 1

--- mycode.py
+++ mycode.py
@@ -3,4 +3,6 @@ import numpy as np
 
 
 df =  pd.read_csv("data/sample_data.csv")
-print(df)
+
+df["grade"] = ["A","B" ,"C","A","B" ,"E","G" ,"B","A","C"]
+print(df.to_csv('data/sample_data.csv', index=False))
